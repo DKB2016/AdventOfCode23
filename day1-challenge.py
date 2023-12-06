228b7dd645066edd9632149157fc7c237da19cc8
djclark_ <david.j.clark95@gmail.com>
2023-12-06 14:05:42 -0600

    part a challenge 1

---
+++ day1-challenge.py
@@ -0,0 +1,38 @@
+#
+#
+#
+#
+import re
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+    new_lines = ' '.join(lines).replace('\n','').split()
+    print(new_lines)
+
+def find_first_last_occurrences(lines):
+
+        
+    results = []
+
+    for line in new_lines:
+        # Find all occurrences of numbers in the string
+        numbers = re.findall(r'\d', line)
+
+        if numbers:
+            # Extract the first and last occurrences of a number
+            first_occurrence = (numbers[0])
+            last_occurrence = (numbers[-1])
+
+            results.append(first_occurrence + last_occurrence)
+        else:
+            # Handle the case where no numbers are found in the string
+            results.append(None)
+
+    return results
+
+
+occurrences = find_first_last_occurrences(lines)
+sum = 0
+for occurrence in occurrences:
+    sum = sum + int(occurrence)
+print(sum)
+print(occurrences)
